9deca48aa11586a453ddf7f7dc2bca9066191c6d
Julian Musshoff <julian.musshoff@rwth-aachen.de>
2020-08-26 19:48:21 +0200

    First commit

---
+++ Header.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_style("darkgrid")
+sns.set_context("talk")
+font = {'family' : 'serif',
+        'weight' : 'normal',
+         'size'   : 18}
+
+plt.rc('font', **font)
+import pandas as pd
